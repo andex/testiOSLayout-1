dbd21e562dd4177bf890945fcbe5c2f3a4550bf0
Andrey <andrey@macbook-pro-maksim-2.local>
2017-07-19 18:18:50 +0300

    — Добавил функцию для градиента — Добавил функцию для line height — Заверстал верхнюю часть лайаута

--- testiOSLayout-1/ViewController.swift
+++ testiOSLayout-1/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gradientView: UIView!
+    @IBOutlet weak var descriptionText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        makeGradienLayer(gradientView)
+        setLineHeight()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func makeGradienLayer(_ gradienView: UIView?) {
+        let gradient = CAGradientLayer()
+        gradient.frame = gradientView.bounds
+        
+        let color1 = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.0).cgColor
+        let color2 = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.5).cgColor
+        gradient.colors = [color1, color2]
+        gradient.locations = [0.0,1.0]
+        
+        gradientView.layer.addSublayer(gradient)
+    }
+    func setLineHeight() {
+        
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 1.5
+        let attrString = NSMutableAttributedString(string: descriptionText.text!)
+        attrString.addAttribute(NSParagraphStyleAttributeName, value:paragraphStyle, range:NSMakeRange(0, attrString.length))
+        descriptionText.attributedText = attrString
+        descriptionText.textAlignment = NSTextAlignment.left
+    }
 
 }
 
